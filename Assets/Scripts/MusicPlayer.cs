943017418fccbf4a301790a258bc90b767b6ffc9
ethranes <nwilcox@hotmail.co.uk>
2016-05-29 19:27:42 +0100

    29051927
    Fixed animation issue
    Added main menu background

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -2,23 +2,24 @@
 using System.Collections;
 
 public class MusicPlayer : MonoBehaviour {
+	
 	static MusicPlayer instance = null;
 
-	// Use this for initialization
+	// a static has been set up otherwise everytime that the game is restarted a new music player would be made, with this code, the new music player
+	// is destoryed when the game is restarted, preventing a music track from playing twice and overlapping.
 	void Start () {
-		if (instance != null) {
+		
+		if (instance != null) 
+		{
 			Destroy (gameObject);
 			print ("Duplicate music player self-destructing!");
-		} else {
-			instance = this;
-			GameObject.DontDestroyOnLoad (gameObject);
-		}
-
-	
+		} else 
+			{
+				instance = this;
+				GameObject.DontDestroyOnLoad (gameObject);
+			}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{	
 	}
 }
