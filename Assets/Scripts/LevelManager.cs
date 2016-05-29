31136faf5cc67e452b290e75719972e6215eec81
ethranes <nwilcox@hotmail.co.uk>
2016-05-29 18:19:21 +0100

    29051818
    new scenes
    level manager

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,15 +1,16 @@
 ﻿using UnityEngine;
-using UnityEngine.SceneManagement;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
-	public AudioSource source;
-	public void LoadLevel(string name){
-		Debug.Log("level load requested for: " + name);
-		//source.Play ();
-		SceneManager.LoadScene (name);
 
-		Cursor.visible = true;
+	public void LoadLevel(string name){
+		Debug.Log ("Level load requested for: "+name);
+		SceneManager.LoadScene (name);
 	}
 
+	public void QuitRequest(){
+		Debug.Log("I want to quit!");
+		Application.Quit ();
+	}
 }
