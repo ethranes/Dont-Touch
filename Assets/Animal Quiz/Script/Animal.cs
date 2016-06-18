7f10eb1f16efea142f64c2219dedc2838aaee82b
ethranes <nwilcox@hotmail.co.uk>
2016-06-18 16:03:41 +0100

    180620161602
    - Removing redundant files

--- Assets/Animal Quiz/Script/Animal.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-
-public class Animal : MonoBehaviour {
-	public string Name;
-	public List<Sprite> AnimalImages = new List<Sprite> ();
-	public AudioClip voice;
-}
