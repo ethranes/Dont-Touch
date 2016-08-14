cdc62264dfd557164cebd1462e466c2e6f035bee
ethranes <nwilcox@hotmail.co.uk>
2016-08-14 03:42:24 +0100

    0342 14/08
    Working on level5 animation

---
+++ Assets/colourSwitchSprite.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class colourSwitchSprite : MonoBehaviour {
+
+	public Sprite GreenCircle;
+	public Sprite WhiteCircle;
+
+	float timer = 1f;
+	float delay = 1f;
+
+	void Start()
+	{
+		this.gameObject.GetComponent<SpriteRenderer> ().sprite = GreenCircle;
+	}
+
+	void Update(){
+		timer -= Time.deltaTime;
+		if (timer <= 0) {
+			if (this.gameObject.GetComponent<SpriteRenderer> ().sprite == GreenCircle) 
+			{
+				this.gameObject.GetComponent<SpriteRenderer> ().sprite = WhiteCircle;
+				timer = delay;
+				return;
+			}
+		}
+		if (this.gameObject.GetComponent<SpriteRenderer> ().sprite == WhiteCircle) 
+		{
+			this.gameObject.GetComponent<SpriteRenderer> ().sprite = GreenCircle;
+			timer = delay;
+			return;
+		}
+	}
+
+	}
+
+
+
+
