217c801f0793f0835a9a19d79b47886e930f7d4e
ethranes <nwilcox@hotmail.co.uk>
2016-08-14 01:18:09 +0100

    0117 14/08
    Finished animation for level 1
    Added Facebook SDK to project

---
+++ Assets/Scripts/plateCrack.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class plateCrack : MonoBehaviour {
+
+	public Sprite plate, setting;
+	private SpriteRenderer spriteRenderer;
+
+
+
+	// Use this for initialization
+	void Start () {
+		spriteRenderer = gameObject.GetComponent<SpriteRenderer> ();
+		SetVolume(PlayerPrefs.GetInt("volume", 50) != 50 ? false : true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ( isTouched ()) {
+			SetVolume (PlayerPrefs.GetInt ("volume", 50) != 50 ? true : false);
+		}	
+	}
+
+	private void SetVolume(bool isOn) {
+		if (isOn) {
+			spriteRenderer.sprite = plate;
+	
+			//PlayerPrefs.SetInt ("volume", 50);
+		} else {
+			spriteRenderer.sprite = setting;
+		
+			//PlayerPrefs.SetInt("volume", 0);
+		}
+	}
+
+	public bool isTouched(){
+		bool result = false;
+		if (Input.touchCount == 1) {
+			if (Input.touches [0].phase == TouchPhase.Ended) {
+				Vector3 wp = Camera.main.ScreenToWorldPoint (Input.GetTouch (0).position);
+				Vector2 touchPos = new Vector2 (wp.x, wp.y);
+				if (GetComponent<Collider2D>() == Physics2D.OverlapPoint (touchPos)) {
+					result = true;
+				}
+			}
+		}
+		if (Input.GetMouseButtonUp (0)) {
+			Vector3 wp = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			Vector2 mousePos = new Vector2 (wp.x, wp.y);
+			if (GetComponent<Collider2D>() == Physics2D.OverlapPoint (mousePos)) {
+				result = true;
+			}
+		}
+		return result;
+	}
+}
---
+++ Assets/spriteScale.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spriteScale : MonoBehaviour {
+
+	public float width = 1;
+	public float height = 1;
+	public Vector3 position = new Vector3( 313, 484, 0 );
+
+	void Awake()
+	{
+		// set the scaling
+		Vector3 scale = new Vector3( width, height, 1f );
+		transform.localScale = scale;
+		// set the position
+		transform.position = position;
+	}
+}
